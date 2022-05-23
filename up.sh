336f96b926f59c54a1c8e39acc876aadda6b4b56
marksfrancis <mrkfrancis8@gmail.com>
2022-05-23 11:56:32 +0000

    Added filebrowser

---
+++ up.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Requires sudo
+# Updates the OS and all docker containers, and then removes legacy unused images
+# Does not automatically restart the host OS if an update requires it
+
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+sudo apt-get autoremove -y
+docker-compose pull
+docker-compose up -d
+docker system prune -a -f
